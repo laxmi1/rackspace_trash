1c8ff742489211c42e8c5db2c480dc33ade0448a
laxmi1 <laxmi@techvoxinc.com>
2014-07-24 16:08:11 +0530

    added script to go last page and click on delete

--- trash_code/rackspace_trash.rb
+++ trash_code/rackspace_trash.rb
@@ -23,12 +23,34 @@ class Login < Test::Unit::TestCase
     @driver.find_element(:name,"user_name").send_keys APPLICATION_CONFIG["user_name"]
     @driver.find_element(:name,"password").send_keys APPLICATION_CONFIG["pass_word"]
     @driver.find_element(:name,"submit_btn").click
-    @driver.find_element(:link_text,"Sent").click
-    sleep(5)
-    @driver.find_element(:link_text,"Inbox").click
+    #wait_for_ajax(@driver)
     sleep(10)
+    @driver.find_element(:xpath,"//img[@class='icon folder_inbox']").click
+    @i = 0
+    while(@i < 2)
+      puts @i
+    @initial = @driver.find_element(:css,".counter>div").text.split(" ")[4].to_i
+    
+    @driver.find_element(:css, ".fill_height.last").click
+    sleep(20)
+    @driver.find_element(:xpath,"//*[@id='wm_core_toolbar']/div/div/ul/li[5]/div/div[2]").click
+    sleep(6)
+    @driver.find_element(:css, ".checkbox>div>input").click
+    #@driver.find_element(:css, ".container.delete").click
+    sleep(10)
+    @actual = @driver.find_element(:css,".counter>div").text.split(" ")[4].to_i
+    puts "actual is : #{@actual} and previous is : #{@initial}"
+    if(@actual != @initial)
+      @cond = true
+      @i = @i+1
+      puts "inside if"
+    else
+      @cond = false
+      puts "inside else"
+    end
+  end
+    #wait_for_ajax(@driver)
 
-    puts "completed"
   end
 
 
